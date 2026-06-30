099f5c88b5250009b20443805778f90a90f2b5c5
supriya-dev705 <supriyarath424@gmail.com>
2026-06-30 23:18:21 +0530

    Implement task addition and management features
    Adds functionality to create and manage tasks with completion and deletion options.

--- Script.js
+++ Script.js
@@ -1,1 +1,31 @@
 
+function addTask() {
+let input = document.getElementById("taskInput");
+let taskText = input.value;
+
+if (taskText === "") {
+    alert("Please enter a task!");
+    return;
+}
+
+let li = document.createElement("li");
+li.innerHTML = taskText;
+
+// Mark complete on click
+li.onclick = function () {
+    li.classList.toggle("completed");
+};
+
+// Delete button
+let deleteBtn = document.createElement("button");
+deleteBtn.innerHTML = "X";
+deleteBtn.onclick = function () {
+    li.remove();
+};
+
+li.appendChild(deleteBtn);
+
+document.getElementById("taskList").appendChild(li);
+input.value = "";
+
+}
